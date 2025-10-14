f6c32db657f2fe3509802604b622d66c45121ea2
Matias Cattaneo <mcattaneo@iagro.global>
2025-10-14 13:13:27 +0000

    word_tags column added for intermediate tables

--- models/intermediate/int_icu_organization.sql
+++ models/intermediate/int_icu_organization.sql
@@ -1,4 +1,3 @@
-
 SELECT
     stg_icu_organization.client_number          AS client_number
     ,stg_icu_organization.legal_name            AS legal_name
@@ -14,18 +13,35 @@ SELECT
     ,int_organization_tag.organization_system   AS organization_system
     ,int_organization_tag.brand                 AS brand
     ,int_organization_tag.founder               AS founder
-    ,int_organization_tag.holding               As holding
+    ,int_organization_tag.holding               AS holding
     ,LOWER(
-            TRANSLATE(
-                CASE 
-                    WHEN CHARINDEX('[', stg_icu_organization.legal_name) > 0
-                        THEN SUBSTR(stg_icu_organization.legal_name, 1, CHARINDEX('[', stg_icu_organization.legal_name) - 1)
-                    ELSE stg_icu_organization.legal_name
-                END,
-                ' .,-_()',
-                ''
+        TRANSLATE(
+            CASE 
+                WHEN CHARINDEX('[', stg_icu_organization.legal_name) > 0
+                    THEN SUBSTR(stg_icu_organization.legal_name, 1, CHARINDEX('[', stg_icu_organization.legal_name) - 1)
+                ELSE stg_icu_organization.legal_name
+            END,
+            ' .,-_()',
+            ''
+        )
+    ) AS icu_literal_name
+    ,REPLACE(
+        TRIM(
+            LOWER(
+                TRANSLATE(
+                    CASE 
+                        WHEN CHARINDEX('[', stg_icu_organization.legal_name) > 0
+                            THEN SUBSTR(stg_icu_organization.legal_name, 1, CHARINDEX('[', stg_icu_organization.legal_name) - 1)
+                        ELSE stg_icu_organization.legal_name
+                    END,
+                    '.,-_()/]',
+                    ''
+                )
             )
-        ) AS icu_literal_name
+        ),
+        ' ',
+        ','
+    ) AS word_tags
 FROM {{ ref('stg_icu_crm_organization') }} AS stg_icu_organization
 INNER JOIN {{ ref('int_icu_organization_tag') }} AS int_organization_tag
     ON stg_icu_organization.id = int_organization_tag.organization_id
