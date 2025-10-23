accf888750fef9cdce56b6f7f9e7dc30cd4dab7a
Matias Cattaneo <mcattaneo@iagro.global>
2025-10-23 21:24:50 +0000

    added: literal_name macro; modified int_ & customer models to use macros - version2 without issues

--- models/intermediate/int_gcms_organization.sql
+++ models/intermediate/int_gcms_organization.sql
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-WITH stg_gcms_companies_src AS (
+WITH source_data AS (
     SELECT
           client_number
         , name                                   AS legal_name
@@ -10,50 +9,11 @@ WITH stg_gcms_companies_src AS (
         , country_code
         , {{ literal_name("name") }}             AS gcms_literal_name
         , {{ word_tags("name") }}                AS word_tags
-=======
-
-WITH stg_gcms_companies AS (
-    SELECT
-        client_number
-        , name                          AS legal_name
-        , trade_name                    AS name
-        , economic_group
-        , parent_company
-        , country                       AS country_en
-        , country_code
-        , LOWER(
-            TRANSLATE(
-                CASE 
-                    WHEN CHARINDEX('[', name) > 0
-                        THEN SUBSTR(name, 1, CHARINDEX('[', name) - 1)
-                    ELSE name
-                END
-                , ' .,-_()'
-                , ''
-            )
-        ) AS gcms_literal_name
-        , REPLACE(
-            LOWER(
-                TRANSLATE(
-                    CASE 
-                        WHEN CHARINDEX('[', name) > 0
-                            THEN SUBSTR(name, 1, CHARINDEX('[', name) - 1)
-                        ELSE name
-                    END
-                    , '.,-_()/]'
-                    , ''
-                )
-            )
-            , ' '
-            , ','
-        ) AS word_tags
->>>>>>> d2d9a082da8111719f22f8571285f1a951b477e8
         , source_companies
     FROM {{ ref('stg_gcms_companies') }}
 )
 
 SELECT
-<<<<<<< HEAD
       client_number
     , legal_name
     , name
@@ -64,8 +24,4 @@ SELECT
     , gcms_literal_name
     , word_tags
     , source_companies
-FROM stg_gcms_companies_src
-=======
-    DISTINCT *
-FROM stg_gcms_companies
->>>>>>> d2d9a082da8111719f22f8571285f1a951b477e8
+FROM source_data
