f31101019eaa1443131092f6e9af92b3e759f01b
Matias Cattaneo <mcattaneo@iagro.global>
2025-10-23 21:08:39 +0000

    fix on int_gcms_org

--- models/intermediate/int_gcms_organization.sql
+++ models/intermediate/int_gcms_organization.sql
@@ -1,71 +1,27 @@
-WITH source_data AS (
+WITH stg_gcms_companies_src AS (
     SELECT
-          stg_icu_organization.client_number                                AS client_number
-        , stg_icu_organization.legal_name                                   AS legal_name
-        , stg_icu_organization.legal_number                                 AS legal_number
-        , stg_icu_organization.name                                         AS name
-        , stg_icu_organization.tax_number                                   AS tax_number
-        , stg_icu_organization.website                                      AS website
-        , stg_icu_organization.id                                           AS organization_id
-        , stg_icu_organization.approved                                     AS approved
-        , stg_icu_organization.approved_at                                  AS approved_at
-        , int_organization_tag.organization_type                            AS organization_type
-        , int_organization_tag.status                                       AS status
-        , int_organization_tag.organization_system                          AS organization_system
-        , int_organization_tag.brand                                        AS brand
-        , int_organization_tag.founder                                      AS founder
-        , int_organization_tag.holding                                      AS holding
-        , {{ literal_name("stg_icu_organization.legal_name") }}             AS icu_literal_name
-        , {{ word_tags("stg_icu_organization.legal_name") }}                AS word_tags
-        , {{ literal_name("stg_icu_organization.tax_number") }}             AS icu_tax_number_literal
-    FROM {{ ref('stg_icu_crm_organization') }} AS stg_icu_organization
-    INNER JOIN {{ ref('int_icu_organization_tag') }} AS int_organization_tag
-        ON stg_icu_organization.id = int_organization_tag.organization_id
-)
-
-, seed_country AS (
-    SELECT
-          code
-        , country_en
-        , country_es
-    FROM {{ ref('country') }}
-)
-
-, address AS (
-    SELECT
-          stg_icu_address.party_id
-        , stg_icu_address.city
-        , stg_icu_address.country_iso_code
-        , stg_icu_address.state_iso_code
-        , stg_icu_address.type
-        , stg_icu_address.zip
-    FROM {{ ref('stg_icu_crm_address') }} AS stg_icu_address
-    WHERE is_primary
+          client_number
+        , name                                   AS legal_name
+        , trade_name                             AS name
+        , economic_group
+        , parent_company
+        , country                                AS country_en
+        , country_code
+        , {{ literal_name("name") }}             AS gcms_literal_name
+        , {{ word_tags("name") }}                AS word_tags
+        , source_companies
+    FROM {{ ref('stg_gcms_companies') }}
 )
 
 SELECT
-      source_data.client_number
-    , source_data.legal_name
-    , source_data.legal_number
-    , source_data.name
-    , source_data.tax_number
-    , source_data.website
-    , source_data.organization_id
-    , source_data.approved
-    , source_data.approved_at
-    , source_data.organization_type
-    , source_data.status
-    , source_data.organization_system
-    , source_data.brand
-    , source_data.founder
-    , source_data.holding
-    , source_data.icu_literal_name
-    , source_data.word_tags
-    , seed_country.country_en                         AS country_en
-    , address.country_iso_code                        AS country_code
-    , source_data.icu_tax_number_literal
-FROM source_data
-LEFT JOIN address
-    ON source_data.organization_id = address.party_id
-LEFT JOIN seed_country
-    ON seed_country.code = address.country_iso_code
+      client_number
+    , legal_name
+    , name
+    , economic_group
+    , parent_company
+    , country_en
+    , country_code
+    , gcms_literal_name
+    , word_tags
+    , source_companies
+FROM stg_gcms_companies_src
